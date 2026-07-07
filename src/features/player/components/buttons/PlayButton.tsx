235256bf7385cc27b104fb04abd9708397e2c51a
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-07 17:54:31 -0300

    feat(ui): aprimora PlayButton com estado interno e estilo circular
    Evolui o componente PlayButton adicionando estado interno (isActive) e uma função toggleActive para gerenciar a interação local. Atualiza a estilização para um layout circular com sombra (shadow), aplicando classes utilitárias do Tailwind para melhor posicionamento e alinhamento do ícone.

--- src/features/player/components/buttons/PlayButton.tsx
+++ src/features/player/components/buttons/PlayButton.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 interface PlayButtonProps {
     isPlaying: boolean;
     onClick: () => void;
 }
 
-function PlayButton({ isPlaying, onClick }: PlayButtonProps) {
+export function PlayButton({ isPlaying, onClick }: PlayButtonProps) {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleActive = () => {
+        onClick();
+        setIsActive(!isActive);
+    }
+
     return (
         <button
-            className={`play-button ${isPlaying ? 'playing' : ''} text-green hover:text-green-hover`}
-            onClick={onClick}
+            className={`w-9 h-9 flex justify-center align-middle rounded-full play-button shadow-[0_4px_9.4px_0_rgba(0,0,0,0.28)] ${isPlaying ? 'playing' : ''} text-green hover:text-green-hover`}
+            onClick={toggleActive}
         >
-            <img className="w-6 h-6" src={isPlaying ? "/icons/pause.svg" : "/icons/play.svg"} alt={isPlaying ? "Pause" : "Play"} />
+            <img className="w-auto h-auto" src={isPlaying ? "/icons/pause.svg" : "/icons/play.svg"} alt={isPlaying ? "Pause" : "Play"} />
         </button>
     );
 }
