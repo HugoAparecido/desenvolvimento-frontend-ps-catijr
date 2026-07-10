32d0d6d973eb897428716fed17c6a4bf78e7e40f
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-10 19:13:32 -0300

    feat(ui): torna ícone de lupa clicável e vincula ao foco do input na SearchBar
    Aprimora a funcionalidade da SearchBar tornando o ícone de busca um elemento interativo. Implementa a lógica para que o clique na lupa atribua foco automaticamente ao campo de input, melhorando a acessibilidade e a eficiência na interação do usuário.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import { FollowingButton } from "./components/ui/buttons/FollowingButton";
-import { ArtistPopularSongs } from "./features/artist/components/ArtistPopularSongs";
-import { SearchBar } from "./features/explore/components/search/SearchBar";
-import { RightClickMusicOptions } from "./features/music/components/action/RightClickMusicOptions";
-
 function App() {
-
   return <>
     <div className="bg-black">
-      <RightClickMusicOptions />
-      <ArtistPopularSongs artistId={1} />
-      <FollowingButton isFollowing={true} onClick={() => console.log("Click")} unfollow={false} />
-      <SearchBar onSearch={(test) => console.log(test)} />
     </div>
   </>;
 }
--- src/features/explore/components/search/SearchBar.tsx
+++ src/features/explore/components/search/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSearchBar } from "../../hooks/useSearchBar.ts";
 
 interface SearchBarProps {
@@ -9,19 +9,36 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
     const [selectedInput, setSelectedInput] = useState<string>('');
     const [closeHovered, setCloseHovered] = useState<boolean>(false);
 
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleImageClick = () => {
+        if (inputRef.current)
+            inputRef.current.focus();
+    }
+
     const { query, handleInputChange, handleSubmit, handleClear } = useSearchBar((q: string) => {
         const mockData = ["Flutter", "Raect", "Vue", "Angular", "Svelte"];
-        const results = mockData.filter(item => item.toLowerCase().includes(q.toLowerCase()));
+        const results = mockData.filter(item =>
+            item.toLowerCase()
+                .includes(q.toLowerCase()));
         onSearch(results);
     });
 
     return (
         <form onSubmit={handleSubmit} className="flex items-center gap-2">
-            <div className="relative w-full max-w-88.75 h-8 flex items-center justify-center rounded-2xl bg-background-highlight">
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3.5 text-gray-400"><img src={selectedInput === 'search' ? "/search_bar/search_active.svg" : "/search_bar/search.svg"} alt="Search" className="h-4" /></span>
+            <div className="relative w-full max-w-88.75 h-8 flex items-center justify-center rounded-2xl bg-background-highlight cursor-text">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3.5 text-gray-400" onClick={handleImageClick}>
+                    <img
+                        src={selectedInput === 'search' ? "/search_bar/search_active.svg" : "/search_bar/search.svg"}
+                        alt="Search"
+                        className="h-4"
+
+                    />
+                </span>
                 <input
                     type="text"
                     placeholder="O que você quer ouvir?"
+                    ref={inputRef}
                     value={query}
                     onChange={handleInputChange}
                     onFocus={() => setSelectedInput('search')}
@@ -29,8 +46,18 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
                     className={`w-full h-full py-2 pl-9.5 pr-5.5 bg-transparent text-sm border ring-1 ${query ? 'ring-text-base' : 'ring-background-highlight'} border-background-highlight rounded-2xl focus:outline-none focus:border-text-base focus:ring-text-base ${query ? 'text-text-base' : 'text-text-subdued'} placeholder-text-subdued transition-colors`}
                 />
                 {query && (
-                    <button type="button" className="absolute inset-y-0 right-0 flex items-center pr-3.5" onClick={handleClear}
-                    ><img src={closeHovered ? "/search_bar/close_active.svg" : "/search_bar/close.svg"} alt="Close" className="h-4 cursor-pointer" onMouseEnter={() => setCloseHovered(true)} onMouseLeave={() => setCloseHovered(false)} /></button>
+                    <button type="button"
+                        className="absolute inset-y-0 right-0 flex items-center pr-3.5"
+                        onClick={handleClear}
+                    >
+                        <img src={closeHovered ? "/search_bar/close_active.svg" : "/search_bar/close.svg"}
+                            alt="Close"
+                            className="h-4 cursor-pointer"
+                            onMouseEnter={
+                                () => setCloseHovered(true)}
+                            onMouseLeave={
+                                () => setCloseHovered(false)} />
+                    </button>
                 )}</div>
         </form>
     );
