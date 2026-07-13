f33871711465dd2356cf7c25e33f67e92ecd6eab
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-12 21:08:54 -0300

    refactor(app): remove instâncias de teste e componentes de sandbox do App.tsx
    Remove instâncias de teste do componente ItemLargeCard e outros elementos temporários do arquivo App.tsx. Limpa a estrutura de visualização do ambiente de desenvolvimento, preparando o arquivo para a implementação das rotas e layouts reais da aplicação.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,5 @@
-import { ItemLargeCard } from "./components/card/ItemLargeCard";
-
 function App() {
   return <>
-    <div className="flex flex-col gap-4 bg-black">
-      {/* Instância 1: Album */}
-      <ItemLargeCard
-        typeCard="Album"
-        text="Random Access Memories"
-        imagePath="/card/album.png"
-        imageDescription="Capa do álbum"
-        albumYear="2013"
-        playAction={() => console.log("Play Album")}
-      />
-
-      {/* Instância 2: Playlist */}
-      <ItemLargeCard
-        typeCard="Playlist"
-        text="Daily Mix 1"
-        imagePath={["/card/playlist1.png", "/card/playlist2.png", "/card/playlist3.png", "/card/playlist4.png",]}
-        imageDescription="Capa da playlist"
-        playlistOwner="Spotify"
-        playAction={() => console.log("Play Playlist")}
-      />
-
-      {/* Instância 3: Artist */}
-      <ItemLargeCard
-        typeCard="Artist"
-        text="Daft Punk"
-        imagePath="/card/artist.png"
-        imageDescription="Foto do artista"
-        playAction={() => console.log("Play Artist")}
-      />
-
-      {/* Instância 4: Person */}
-      <ItemLargeCard
-        typeCard="Person"
-        text="Nome do Usuário"
-        imagePath="/card/person.png"
-        imageDescription="Foto de perfil"
-        playAction={() => console.log("Play Person")}
-      />
-    </div>
   </>;
 }
 
