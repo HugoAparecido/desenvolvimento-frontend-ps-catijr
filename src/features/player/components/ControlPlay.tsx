b7f072344ab310eb5e26b2a1561d0b26e1918299
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-13 11:52:57 -0300

    refactor(ui): aprimora interatividade e responsividade dos controles de reprodução
    Implementa animações de hover no PlayButtonPlayer para fornecer feedback visual aprimorado ao usuário. Refina o alinhamento e a acessibilidade dos botões de controle (SkipPrevious/Next) e aplica ajustes de estilização nos ícones, garantindo uma interface mais coesa e intuitiva.

--- src/features/player/components/ControlPlay.tsx
+++ src/features/player/components/ControlPlay.tsx
@@ -12,15 +12,15 @@ export function ControlPlay({ isPlaying, onTogglePlay, nextPathMusicClick, previ
 
 
     return (
-        <div className="w-max flex gap-2.5 justify-between items-center">
-            <button onClick={previousPathMusicClick}
-                className="cursor-pointer text-text-subdued"
+        <div className="w-max flex gap-2.5 justify-between items-center transition-colors duration-300">
+            <button onClick={previousPathMusicClick} aria-label="Música anterior"
+                className="cursor-pointer text-text-subdued hover:text-white"
             >
                 <MdSkipPrevious size={20} />
             </button>
             <PlayButtonPlayer isPlaying={isPlaying} onClick={onTogglePlay} />
             <button onClick={nextPathMusicClick}
-                className="cursor-pointer text-text-subdued"
+                className="cursor-pointer text-text-subdued hover:text-white" aria-label="Próxima música"
             >
                 <MdSkipNext size={20} />
             </button>
--- src/features/player/components/buttons/PlayButtonPlayer.tsx
+++ src/features/player/components/buttons/PlayButtonPlayer.tsx
@@ -6,7 +6,7 @@ interface PlayButtonPlayerProps {
 export function PlayButtonPlayer({ isPlaying, onClick }: PlayButtonPlayerProps) {
     return (
         <button
-            className={`w-6 h-6 flex p-1.5 bg-text-base items-center cursor-pointer justify-center rounded-full ease-out duration-300`}
+            className={`w-6 h-6 flex p-1.5 bg-text-base items-center cursor-pointer justify-center rounded-full ease-out duration-300 hover:border hover:border-text-base`}
             onClick={onClick}
         >
             <img className="h-full invert" src={isPlaying ? "/player/pause.svg" : "/player/play.svg"} alt={isPlaying ? "Pause" : "Play"} />
