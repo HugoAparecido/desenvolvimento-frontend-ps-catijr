51d37fda5633ba7927972886f61ac004f3adee60
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-20 21:51:19 -0300

    feat(library): integra sistema de filtros e busca dinâmica na barra lateral
    Composição do Painel: Atualiza o componente Library para orquestrar os estados globais da sidebar, unindo LibraryFilter, LibrarySearch e LibraryBarItem.
    
    Gerenciamento de Estado: Introduz o estado currentFilter baseado no tipo tipado LibraryFilterValue, permitindo alternar dinamicamente os filtros de exibição ('all', 'playlist', 'album', 'artist').
    
    Dinamicidade de Busca: Conecta o componente de filtro aos botões de interface (FilterButton) através de um dicionário de exibição (libraryFilterValueDisplayNames), garantindo consistência semântica e visual.
    
    Refinamento: Passa a propriedade currentFilter corretamente para o LibraryBarItem, preparando o terreno para que o componente reaja aos termos digitados na busca e aos filtros selecionados.

--- src/features/library/Library.tsx
+++ src/features/library/Library.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import { LibraryBarItem } from "./components/bar/LibraryBarItem";
+import { LibraryFilter } from "./components/filter/LibraryFilter";
 import { LibrarySearch } from "./components/search/LibrarySearch";
+import type { LibraryFilterValue } from "./hooks/useLibraryFilter";
 
 export function Library() {
+    const [currentFilter, setCurrentFilter] = useState<LibraryFilterValue>('all');
+
     return (
         <div className="flex flex-col w-max h-full pb-3 gap-3 overflow-y-scroll items-center justify-start bg-background-base rounded-lg [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] scrollbar-none">
             <div className="flex w-max h-max flex-col justify-start items-center gap-3 p-3">
+                <LibraryFilter selectedFilter={currentFilter} onSelectFilter={setCurrentFilter} />
                 <LibrarySearch />
             </div>
             <LibraryBarItem
                 query=""
-                filter="all"
+                filter={currentFilter}
             />
         </div>
     )
