81bcc242abb53468dadf8a931e56a2fc0e9de4ad
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-21 10:06:22 -0300

    feat(player): implementa modo tela cheia (SongFull) e integração reativa no AppLayout
    Visualização Expandida: Cria o componente SongFull para renderizar a capa da música atual em tela cheia, aplicando gradientes customizados de fundo (--background-image-song-full) e sombra projetada (--shadow-song-full).
    
    Gerenciamento de Layout Global: Atualiza o AppLayout para ler o estado isFullScreen via usePlayerStore, ocultando dinamicamente a Navbar, a Library (sidebar) e o SongPanel quando o modo tela cheia estiver ativo.
    
    Controles de Alternância: Integra o botão de expansão/contração no player (VolumeControl) utilizando ícones direcionais (GoScreenFull / GoScreenNormal) e vinculando a ação ao método toggleFullScreen e à rota correspondente (song/).

---
+++ src/pages/SongFull.tsx
@@ -0,0 +1,14 @@
+import { usePlayerStore } from "../features/player/store/usePlayerStore"
+
+export function SongFull() {
+    const currentTrack = usePlayerStore((state) => state.currentTrack);
+
+    return (
+        <div className="w-full h-full flex m-1 p-2.5 gap-2.5 items-center justify-center bg-song-full rounded-2xl">
+            <img
+                src={currentTrack?.imagePath} alt="Album image"
+                className="w-full h-auto aspect-square max-w-157.25 rounded-2xl object-cover shadow-song-full"
+            />
+        </div>
+    )
+}
