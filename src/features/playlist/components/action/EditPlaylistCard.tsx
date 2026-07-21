7138c7a36a1a6406c80b53d5cee5612f8db5e5af
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-21 16:00:21 -0300

    feat(playlist): implementa componente EditPlaylistCard e integração com o menu de contexto
    Modal de Edição de Playlist: Cria o componente EditPlaylistCard permitindo a alteração de nome, descrição e visibilidade (pública/privada) da playlist, com suporte a layout dinâmico de capas (1 imagem ou grade 2x2).
    
    Integração de Contexto: Conecta a opção 'Editar os detalhes' no RightClickPlaylistOptions para disparar dinamicamente o modal via sistema global de usePopup.
    
    Tratamento de Formulários: Adiciona estados locais controlados para os campos de entrada e botão de submissão com prevenção de recarregamento padrão (e.preventDefault()).
    
    Ajustes de UI: Inclui aviso legal de upload de imagens e estruturação visual alinhada com o design system existente.

---
+++ src/features/playlist/components/action/EditPlaylistCard.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Button } from "../../../../components/ui/buttons/Button";
+
+interface EditPlaylistCardProps {
+    playlist: {
+        id: string | number,
+        imagePath: string | string[],
+        name: string,
+        description: string,
+        isPublic: boolean,
+    }
+    onSaveClick: () => void
+}
+
+export function EditPlaylistCard({ playlist }: EditPlaylistCardProps) {
+    const [nameValue, setNameValue] = useState(playlist.name);
+    const [descriptionValue, setDescriptionValue] = useState(playlist.description);
+    const [isPublicValue, setIsPublicValue] = useState(playlist.isPublic);
+
+    const normalizedImage = Array.isArray(playlist.imagePath) ? playlist.imagePath : [playlist.imagePath];
+    const quantitity = normalizedImage.length;
+    const imagesForRendering = quantitity === 1 ? normalizedImage : Array.from({ length: 4 }).map((_, index) => normalizedImage[index] || null)
+
+    const gridClass = quantitity === 1 ? 'grid-cols-1' : 'grid-cols-2 grid-rows-2';
+    const imageClass = 'w-full h-full';
+
+    const handleInputNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNameValue(e.target.value);
+    }
+
+    const handleInputDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescriptionValue(e.target.value);
+    }
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form className="flex flex-col w-max h-max rounded-lg p-5 gap-4 bg-popup-bg"
+            onSubmit={handleSubmit}
+        >
+            <span className="text-base text-text-base lining-none font-bold font-default-font">
+                Editar detalhes
+            </span>
+            <div className="flex w-max h-max gap-3 items-center">
+                <div className={`relative w-33.5 h-33.5 grid overflow-hidden rounded-sm ${gridClass}`}>{imagesForRendering.map((img, index) => {
+                    if (img) {
+                        return (
+                            <img
+                                key={index}
+                                src={img}
+                                alt={`Capa album ${index + 1}`}
+                                className={`${imageClass} object-cover block`} />
+                        )
+                    }
+                    return (
+                        <div
+                            key={`empty-${index}`}
+                            className={`${imageClass} bg-black`}
+                        />
+                    );
+                })}
+                </div>
+                <div className="flex w-max gap-2 flex-col">
+                    <input type="text"
+                        className="w-52.5 h-7.5 rounded-sm gap-2.5 p-2 bg-textbox-bg text-xs font-default-font font-medium lining-none text-white
+                        focus:outline-none
+                        "
+                        onChange={handleInputNameChange}
+                        value={nameValue}
+                    />
+                    <textarea
+                        className="w-52.5 h-23.25 rounded-sm gap-2.5 p-2 bg-textbox-bg text-xs font-default-font font-medium lining-none text-white
+                        focus:outline-none resize-none [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] scrollbar-none
+                        "
+                        onChange={handleInputDescriptionChange}
+                        value={descriptionValue}
+
+                    />
+                </div>
+            </div>
+            <div className="flex w-full justify-between items-start">
+                <Button text={isPublicValue ? "Tornar privada" : "Tornar pública"}
+                    onClick={() => setIsPublicValue(!isPublicValue)}
+                />
+                <Button variant="CTA" text="Salvar" withIcon={false} />
+            </div>
+            <span className="w-89 text-[8px] font-default-font font-bold lining-none text-white">Ao continuar, você autoriza o Spotify a acessar a imagem enviada. Certifique-se de que você tem o direito de fazer o upload dessa imagem.</span>
+        </form>
+    )
+}
--- src/features/playlist/components/action/RightClickPlaylistOptions.tsx
+++ src/features/playlist/components/action/RightClickPlaylistOptions.tsx
@@ -1,30 +1,48 @@
+import { usePopup } from "../../../../components/popup/hook/usePopup";
 import { RightClickMenu } from "../../../../components/ui/options/RightClickMenu";
 import type { RightClickMenuNode } from "../../../../components/ui/options/RightClickMenuItem";
-
-const optionItems: RightClickMenuNode[] = [
-    {
-        iconPath: "/action/pencil.svg",
-        iconDescription: "Pencil",
-        text: "Editar os detalhes",
-    }, {
-        iconPath: "/action/block.svg",
-        iconDescription: "Block",
-        text: "Apagar playlist",
-    }, {
-        iconPath: "/action/lock.svg",
-        iconDescription: "Lock",
-        text: "Tornar particular",
-    }, {
-        iconPath: "/action/lock.svg",
-        iconDescription: "Lock",
-        text: "Tornar pública",
-    }, {
-        iconPath: "/tag/pin.svg",
-        iconDescription: "Pin",
-        text: "Fixar playlist",
-    },
-];
+import { EditPlaylistCard } from "./EditPlaylistCard";
 
 export function RightClickPlaylistOptions() {
+    const { openPopup, closePopup } = usePopup();
+
+    const optionItems: RightClickMenuNode[] = [
+        {
+            iconPath: "/action/pencil.svg",
+            iconDescription: "Pencil",
+            text: "Editar os detalhes",
+            onClick: () => openPopup(
+                <EditPlaylistCard
+                    playlist={{
+                        id: 1,
+                        description: "Descrição da playlist preenchida",
+                        imagePath: ["/card/playlist1.png", "/card/playlist2.png", "/card/playlist3.png", "/card/playlist4.png"],
+                        isPublic: true,
+                        name: "you know",
+                    }}
+                    onSaveClick={() => {
+                        closePopup();
+                    }}
+                />
+            ),
+        }, {
+            iconPath: "/action/block.svg",
+            iconDescription: "Block",
+            text: "Apagar playlist",
+        }, {
+            iconPath: "/action/lock.svg",
+            iconDescription: "Lock",
+            text: "Tornar particular",
+        }, {
+            iconPath: "/action/lock.svg",
+            iconDescription: "Lock",
+            text: "Tornar pública",
+        }, {
+            iconPath: "/tag/pin.svg",
+            iconDescription: "Pin",
+            text: "Fixar playlist",
+        },
+    ];
+
     return <RightClickMenu options={optionItems} />
 }
