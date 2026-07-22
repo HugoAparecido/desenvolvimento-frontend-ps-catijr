f536a9923be68136479a698fa36d440b1ce9d8c7
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-07-22 16:04:23 -0300

    feat(profile): implementa busca e exibição dinâmica de artistas mais tocados no perfil do usuário
    Interfaces e Serviço de Artistas: Adiciona a interface MostPlayedArtists e implementa o método assíncrono getMostPlayedArtists no artistService para consumir o endpoint de artistas mais tocados.
    
    TanStack Query: Desenvolve o hook customizado useMostPlayedArtists para gerenciar o estado da requisição e cache dos dados de artistas mais tocados.
    
    Página de Perfil (Profile): Integra o hook na página de perfil para renderizar dinamicamente a seção 'Artistas mais tocados este mês' utilizando o componente ItemLargeCard.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,19 +1,38 @@
+import { ItemLargeCard } from "../components/card/ItemLargeCard";
 import { ProfileHeader } from "../features/profile/ProfileHeader";
-
-const mockProfileData = {
-    profile: {
-        name: "Hugo Aparecido",
-        imagePath: "profile/profile.png",
-        qtdPlaylists: 14,
-        qtdFollowers: 1250,
-        qtdFollowing: 345
-    }
-};
+import { useMostPlayedArtists } from "../hooks/useArtist";
+import { mockUser } from "../mockData/mockUserInfos";
 
 export function Profile() {
+    const { data: mostPlayedArtists = [], isLoadingMostPlayedArtists } = useMostPlayedArtists();
+
+
     return (
-        <div className="flex w-full">
-            <ProfileHeader profile={mockProfileData.profile} />
+        <div className="flex w-full flex-col gap-8 bg-home-bg-gradient-variant rounded-xl">
+            <ProfileHeader profile={{
+                imagePath: mockUser.imagePath,
+                name: mockUser.name,
+                qtdFollowers: mockUser.qtdFollowers,
+                qtdFollowing: mockUser.qtdFollowing,
+                qtdPlaylists: mockUser.qtdPlaylists
+            }} />
+            <div className="w-full px-4 gap-6">
+                <div className="gap-1 flex flex-col">
+                    <span className="text-base font-bold font-default-font lining-none text-text-base">Artistas mais tocados este mês</span>
+                    <span className="text-xs font-medium font-default-font lining-none text-text-subdued">Visíveis apenas para você</span>
+                    <div className="flex gap-3">
+                        {mostPlayedArtists.map((artist) => (
+                            <ItemLargeCard
+                                imageDescription="Artist Image"
+                                imagePath="/card/artist.png"
+                                playAction={() => { }}
+                                text={artist.name}
+                                typeCard="Artist"
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/services/artists.service.ts
+++ src/services/artists.service.ts
@@ -1,9 +1,14 @@
 import { api } from "../api/axios"
-import type { RecentArtist } from "../types/artist";
+import type { MostPlayedArtists, RecentArtist } from "../types/artist";
 
 export const artistService = {
     getRecentArtists: async (): Promise<RecentArtist[]> => {
         const response = await api.get<RecentArtist[]>(`/user/recentArtists`);
         return response.data;
-    }
+    },
+
+    getMostPlayedArtists: async (): Promise<MostPlayedArtists[]> => {
+        const response = await api.get<MostPlayedArtists[]>(`/user/mostPlayedArtists`);
+        return response.data;
+    },
 }
